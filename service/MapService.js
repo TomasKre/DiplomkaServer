8e320c54fb0881e58d2671d4fd408401ed98b314
Tomas Krechler <tomas.krechler@tul.cz>
2023-03-27 02:09:22 +0200

    Added - showing data from database in heatmap

--- service/MapService.js
+++ service/MapService.js
@@ -1,12 +1,12 @@
 'use strict';
-/*const mariadb = require('mariadb');
+const mariadb = require('mariadb');
 const pool = mariadb.createPool({
      host: 'localhost',
      database: 'ulice',
      user:'ulice',
      password: 've9ZTyC2I63p3b5M',
      connectionLimit: 5
-});/*
+});
 
 /**
  * Get map data
@@ -15,16 +15,21 @@ const pool = mariadb.createPool({
  **/
 exports.getMap = function () {
   return new Promise(function (resolve, reject) {
-    // return some example heatmap data for now
-    const heatmapData = [
-      { location: { lat: 37.7749, lng: -122.4194 }, weight: 1.5 },
-      { location: { lat: 37.7747, lng: -122.4192 }, weight: 0.5 },
-      { location: { lat: 37.7745, lng: -122.4190 }, weight: 1.5 },
-      { location: { lat: 37.7743, lng: -122.4192 }, weight: 0.5 },
-      { location: { lat: 37.7741, lng: -122.4194 }, weight: 1.5 },
-      // add more heatmap data points here
-    ];
-
-    resolve(heatmapData);
+    pool.getConnection()
+    .then(conn => {
+      conn.query('SELECT lat, lon FROM DataPoints')
+        .then(rows => {
+          console.log(rows); // rows contains rows returned by server
+          conn.release(); // release the connection back to the pool
+          resolve(rows); // send back template with data
+        })
+        .catch(err => {
+          console.log(err);
+          conn.release(); // release the connection back to the pool
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
   });
 };
